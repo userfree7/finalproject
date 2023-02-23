48d140bace563b87bf80ecf18c003b73e6eb2ba3
Oksana <oksanaoksana25@gmail.com>
2023-02-23 16:18:52 +0200

    added Logo.tsx

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material'
+import logo from 'assets/logo.png'
+
+type Props = {}
+
+const Logo = (props: Props) => {
+    return (
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <img src={logo} alt="fake blog" width="90px" height="50px" />
+        </Typography>
+    )
+}
+export default Logo
