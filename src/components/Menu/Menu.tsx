496d4638fa4145e4c82b2b563257815f51c72b3a
Oksana <oksanaoksana25@gmail.com>
2023-02-23 17:20:14 +0200

    added Menu.tsx

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,14 @@
+import Button from '@mui/material/Button'
+
+type Props = {}
+
+const Menu = (props: Props) => {
+    return (
+        <>
+            <Button color="inherit">Practice</Button>
+            <Button color="inherit">Articles</Button>
+            <Button color="inherit">Philosophy</Button>
+        </>
+    )
+}
+export default Menu
